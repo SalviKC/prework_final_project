f4f688950507efe0380bfd137b72e5380c9b75f7
SalviKC <102228468+SalviKC@users.noreply.github.com>
2025-03-12 19:26:08 +0000

    Terminado

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+name = "Salvador"
+age = 27
+like_prog = True
+qual = 8.75
+
+list = [8,2,5,0,16]
+
+student = [{ 
+        "Name": name, 
+        "Edad": age,
+        "Calificación final": qual
+    }]
+
+stu_qual = np.array([8.5, 9.2, 7.8, 8.9, 10])
